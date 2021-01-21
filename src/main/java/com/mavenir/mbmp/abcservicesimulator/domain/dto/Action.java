8c9325b3190abed3e344eb7f798b1a78f52429fc
Reehan <reehan.lalkhan@gmail.com>
2021-01-21 15:52:55 +0530

    New Actions file to fix flow as per ABC

---
+++ src/main/java/com/mavenir/mbmp/abcservicesimulator/domain/dto/Action.java
@@ -0,0 +1,18 @@
+package com.mavenir.mbmp.abcservicesimulator.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Action {
+    private UrlAction urlAction;
+    private Postback postback;
+    private String displayText;
+}
